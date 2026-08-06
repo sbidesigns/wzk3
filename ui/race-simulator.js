1a9d60e11df13e72b9e7a87ce4f733998fcc3fdf
Z User <z@container>
2026-08-06 20:39:23 +0000

    fix: add race-simulator.js to deploy repo to prevent 404 warning

---
+++ ui/race-simulator.js
@@ -0,0 +1,13 @@
+// ui/race-simulator.js - Stub (dev tool removed in Cycle 41)
+// This file exists to prevent 404 errors when main.js tries to import it
+
+export const raceSimulator = {
+  init: function(opts) {
+    console.log('[RaceSimulator] Stub initialized (dev mode disabled)');
+  },
+  start: function() {},
+  stop: function() {},
+  reset: function() {}
+};
+
+export default { raceSimulator };
